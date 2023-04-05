6f5115b96b5bb8335528b5e3ae6c1eaf73bb4918
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-04-05 18:04:55 +1000

    Dev (#2)
    * Initial commit
    
    * Fixing error with terraform

--- model/aws_lambda_layer_version.tf
+++ model/aws_lambda_layer_version.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_layer_version" "model" {
   compatible_runtimes = ["python3.8"]
-  filename            = "AnimalCrossingModel.zip"
+  filename            = data.archive_file.model.output_path
   layer_name          = "AnimalCrossingModel"
   source_code_hash    = filebase64sha256(data.archive_file.model.output_path)
 }
