1f4c2923100e407d3074cef64fd6f1b77e884c28
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-04-05 21:58:10 +1000

    Dev (#3)
    * Initial commit
    
    * Fixing error with terraform
    
    * Added MySQL
    
    * Updated workflows

--- mysql/aws_lambda_layer_version.tf
+++ mysql/aws_lambda_layer_version.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_layer_version" "main" {
   compatible_architectures = ["arm64", "x86_64"]
   compatible_runtimes      = ["python3.7", "python3.8", "python3.9"]
-  description              = "Animal Crossing data models."
+  description              = "Animal Crossing MySQL connector-python."
   filename                 = data.archive_file.main.output_path
-  layer_name               = "AnimalCrossingModel"
+  layer_name               = "AnimalCrossingMySQLConnectorPython"
   skip_destroy             = false
   source_code_hash         = filebase64sha256(data.archive_file.main.output_path)
 }
