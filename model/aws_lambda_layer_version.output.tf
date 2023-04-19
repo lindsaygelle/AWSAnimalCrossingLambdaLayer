c614728884c97a644bb816f7629fd4c00a8fd416
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-04-19 17:48:01 +1000

    Dev (#10)
    * Dev

--- model/aws_lambda_layer_version.output.tf
+++ model/aws_lambda_layer_version.output.tf
@@ -5,3 +5,7 @@ output "arn" {
 output "filename" {
   value = aws_lambda_layer_version.main.filename
 }
+
+output "compatible_runtimes" {
+  value = aws_lambda_layer_version.main.compatible_runtimes
+}
--- mysql/aws_lambda_layer_version.tf
+++ mysql/aws_lambda_layer_version.tf
@@ -1,9 +1,10 @@
 resource "aws_lambda_layer_version" "main" {
   compatible_architectures = ["arm64", "x86_64"]
-  compatible_runtimes      = ["python3.7", "python3.8", "python3.9"]
-  description              = "Animal Crossing MySQL connector-python."
+  compatible_runtimes      = ["python3.8", "python3.9"]
+  description              = "https://github.com/lindsaygelle/AWSAnimalCrossingModel/blob/main/README.md"
   filename                 = data.archive_file.main.output_path
-  layer_name               = "AnimalCrossingMySQLConnectorPython"
+  layer_name               = "AnimalCrossingModel"
+  license_info             = "https://opensource.org/licenses/MIT"
   skip_destroy             = false
   source_code_hash         = filebase64sha256(data.archive_file.main.output_path)
 }
